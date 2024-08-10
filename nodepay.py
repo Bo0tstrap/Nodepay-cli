b20426822e12756c01f3ea187a5c6aadda4bf54b
Bootstrap <66469237+Bo0tstrap@users.noreply.github.com>
2024-08-10 15:03:55 +0300

    Update nodepay.py

--- nodepay.py
+++ nodepay.py
@@ -10,11 +10,6 @@ import sys
 # Script'in bulunduğu dizini belirle
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Tek satırlık token'ı dosyaya yazma fonksiyonu
-def write_single_line_file(file_path, content):
-    with open(file_path, 'w') as f:
-        f.write(content)
-
 # Tek satırlık token'ı dosyadan okuma fonksiyonu
 def read_single_line_file(file_path):
     if os.path.exists(file_path):
@@ -22,9 +17,17 @@ def read_single_line_file(file_path):
             return f.read().strip()
     return None
 
-# Dosyadan yapılandırma değerlerini oku
-token_file_path = os.path.join(script_dir, 'token.txt')
-NP_TOKEN = read_single_line_file(token_file_path)
+# Birden fazla satırı dosyadan okuma fonksiyonu
+def read_lines_file(file_path):
+    with open(file_path, 'r') as f:
+        return f.read().splitlines()
+
+# Boş satırları filtreleme fonksiyonu
+def filter_non_empty_lines(lines):
+    return [line for line in lines if line.strip()]
+
+# Dosyalardan yapılandırma değerlerini oku
+NP_TOKEN = read_single_line_file(os.path.join(script_dir, 'token.txt'))
 
 # Sabitler
 HTTPS_URL = "https://nw2.nodepay.ai/api/network/ping"
@@ -59,4 +62,32 @@ async def send_ping(user_id, token):
             payload = {
                 "user_id": user_id,
                 "browser_id": browser_id,
-                "time
+                "timestamp": int(time.time()),
+                "version": EXTENSION_VERSION
+            }
+            response = requests.post(HTTPS_URL, headers=headers, json=payload)
+            response.raise_for_status()
+            logger.debug(response.json())
+            await asyncio.sleep(10)  # Bir sonraki eylem için biraz bekle
+        except Exception as e:
+            logger.error(e)
+            await asyncio.sleep(RETRY_INTERVAL)
+
+# Ana fonksiyon
+async def main():
+    # Güncelleme kontrolü ve indirme işlemleri kaldırıldı
+
+    # API'den USER_ID'yi al
+    if NP_TOKEN != "":
+        try:
+            user_data = await call_api_info(NP_TOKEN)
+            logger.debug(user_data)
+            USER_ID = user_data['data']['uid']
+            await send_ping(USER_ID, NP_TOKEN)
+        except Exception as e:
+            logger.error(f"USER_ID alınırken hata: {e}")
+    else:
+        logger.error("token.txt içinde NP_TOKEN değeri belirtilmelidir.")
+
+if __name__ == '__main__':
+    asyncio.run(main())
